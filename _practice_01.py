bed7bab891f77f511f484d3d27586ed25d420bf9
Sibuor007 <139122719+Sibuor007@users.noreply.github.com>
2024-01-30 00:17:24 +0300

    Update _practice_01.py

--- _practice_01.py
+++ _practice_01.py
@@ -1,12 +1,14 @@
-invest_rt = input("Enter investment amount:")
+invest_amount = input("Enter investment amount:")
 rate = input("Enter Investment rate: ")
 
-invest_rt = float(invest_rt)
+# typecasting input from user to float
+invest_amount = float(invest_amount)
 # rounds the interest rate to 2 decimal places.
 rate = float(rate) * .01
 
+# 10 iterations for 10 months of investment
 for i in range(10):
-    invest_rt = invest_rt + (invest_rt * rate)
-print("Money invested after 10 years: {:.2f}".format(invest_rt))
-
-
+    # add the interet to the intial investment amount one by one
+    invest_amount += (invest_amount * rate)
+# print the final investment after 10 years using the string.format
+print("Money invested after 10 years: {:.2f}".format(invest_amount))
