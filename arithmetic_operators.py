1e5bde3aed271788992c503b16c877e4e8785ae3
Sibuor007 <139122719+Sibuor007@users.noreply.github.com>
2024-01-30 13:45:27 +0300

    Update arithmetic_operators.py

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,14 +1,22 @@
-
+# lines of code to show arithmetic operations
+# + addition
 print(9 + 23)
+# / division
 print(56 / 23)
+# // floor division-quotient
 print(56 // 23)
+# ** exponent
 print(9 ** 4)
+# modulus-remainder
 print(5 % 2)
+# multiple operations-precedence and associativity
 print(5 + 2 * 3 - 4 + 40/7)
 
+# taking input from users
 weight = input("Enter your weight: ")
 height = input("Enter your height in meters: ")
 # calculate_BMI
 result = (int(weight) / (float(height) * float(height)))
+# print the result in normal fomart
 print("BMI = ", result)
 
