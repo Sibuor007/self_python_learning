aeaae25288eb141a337ed9e0cd3ee196ea4a8a5f
Sibuor007 <139122719+Sibuor007@users.noreply.github.com>
2024-01-30 13:48:39 +0300

    Update assign_operator.py

--- assign_operator.py
+++ assign_operator.py
@@ -1,10 +1,19 @@
+# assigning values to positional variables at once
 a, b, c = 1048, 8383, 928
+# printing the assigned values all at once
 print(a, b, c)
+
+# adding 1234 to the value assigned in a
 a += 1234
+# printing new value in a
 print(a)
+# adding the new value in a to the assigned value in c
 c += a
+# printing the new value in c
 print(c)
 
+# assigning a value to z
 z = 12
+# using a comparison operaiton to check boolean output
 print(z < 23)
 
